0af6bc483b13c1c98f766224f071aa906202937a
firstbit.demo <vhankalasprajakta232002@gmail.com>
2025-11-12 13:21:08 +0530

    delete .exe files, keep .c files

--- Add.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-	int a=30,b=20;
-	int sum=a+b;
-	printf("addition of is a=%d,b=%d, sum=%d\n",a,b,sum);
-}
--- addition1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a=10, b=50;
-
-	int sum=a+b;
-	printf("sum is %d",sum);
-}
